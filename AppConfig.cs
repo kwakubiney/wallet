639ff6bad179b106b4a8819ccdaf584e5d182276
kwakubiney <kebiney@hotmail.com>
2023-11-08 16:39:31 +0000

    fix: delete unecessary file

--- AppConfig.cs
+++
@@ -1,9 +0,0 @@
-public class AppConfig{
-    static public string Issuer {get;set;}
-    static public string Key {get;set;}
-    public AppConfig(IConfiguration config)
-    {
-        Issuer = config.GetValue<string>("Jwt:Issuer");
-        Key = config.GetValue<string>("Jwt:Key");
-    }
-}
